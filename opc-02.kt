9d5a88855c0fbfa0dffba38a81e8a8e7fa5ef0c1
JustaRandomGuy <118472272+M-S-G-A@users.noreply.github.com>
2024-09-21 19:44:07 +0100

    Task 10 done

--- opc-02.kt
+++ opc-02.kt
@@ -2,34 +2,59 @@ import java.util.InputMismatchException
 import java.util.Scanner
 
 
-class Client(val name: String,val meterNumber: String, val consumption: Double){
-    fun calculatebill() = if (consumption>1000) 0.9*consumption*50 else consumption*50
-
-    val bill = calculatebill()
-
+open class Client(val name: String,val meterNumber: Int, val consumption: Double){
+    open val payRate = 50
+    open fun calculateBill() = if (consumption>1000) 0.9*consumption*payRate else consumption*payRate
+    open val bill = calculateBill()
 }
 
+class PrivateClient(name: String, meterNumber: Int, consumption: Double): Client(name, meterNumber, consumption){
+    override val payRate: Int = 45
+    override val bill: Double = super.calculateBill()
+}
+
+class ClientCompany(name: String, meterNumber: Int, consumption: Double): Client(name, meterNumber, consumption){
+    override val payRate: Int = 55
+    override val bill: Double = super.calculateBill()
+}
+
+var clients: MutableList<Client> = mutableListOf()
+
+//fun createPrivate(name: String, meterNumber: Int, consumption: Double){
+//    clients.add(PrivateClient(name,meterNumber, consumption)
+//}
+//
+//fun createCompany(name: String, meterNumber: Int, consumption: Double){
+//    clients.add(ClientCompany(name, meterNumber, consumption))
+//}
+
 fun main(){
-    println("Bienvenue a l'application de facturation d'electricite\n")
 
     val reader = Scanner(System.`in`)
     var nextClient = true
     var name: String
-    var meterNumber: String
+    var meterNumber: Int
     var consumption = 0.0
     var currentClient: Client
-    var clients: MutableList<Client> = mutableListOf()
     var answer = ""
     var sum=0.0
     var average: Double
 
     while (nextClient){
-        print("What's your name?\t")
+        println("Bienvenue a l'application de facturation d'electricite\n")
+
+        println("What would you like to do?")
+        println("1 - Create Private Client")
+        println("2 - Create Company Client")
+        println("3 - Display bills")
+        println("4 - Display average consumption")
+
+        print("\nWhat's your name?\t")
         name = reader.nextLine()
         println()
 
         print("What's your meter number?\t")
-        meterNumber = reader.nextLine()
+        meterNumber = reader.nextInt()
         println()
 
         try {
@@ -49,17 +74,17 @@ fun main(){
         currentClient = Client(name,meterNumber,consumption)
         clients.add(currentClient)
 
-        println("Hello, $name")
+        println("Hello, ${name.capitalize()}.")
         println("Your meter number is $meterNumber and your consumption is $consumption kWh")
 
-        println("Hence, your bill is ${currentClient.bill}\n")
+        println("Hence, your bill is ${currentClient.bill}frs\n")
 //        bills.add(bill)
 
         print("Is there another Client? (Y/N)\t")
         answer = reader.nextLine()
 
         while (answer!= "Y" && answer!="y" && answer!="n" && answer!="N" ){
-            print("Is there another Client? (Y/N)\t")
+//            print("Is there another Client? (Y/N)\t")
             answer = reader.nextLine()
         }
 
@@ -67,11 +92,10 @@ fun main(){
     }
 
     for (client in clients){
-        sum+=client.bill
+        sum += client.bill
     }
     average = sum/clients.size
-    println("\nYour average consumption is $average")
-
+    println("\nThe average consumption is ${average}frs\n")
 
 
 
